8c8591d7734b5b767b9dccb687ea9e6aa331337f
hachem rifaii <107303596+hachem-rifaii@users.noreply.github.com>
2025-03-08 00:43:56 +0200

    get the backend url

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://auth-delta-two-84.vercel.app",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
